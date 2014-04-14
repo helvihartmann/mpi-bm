58320e154e8624070889bd13adc390e7258058b6
Helvi Hartmann <hartmann@cbmfles01.gsi.de>
2014-04-14 10:10:30 +0200

    changed pipeline.sh and moved tests file to the according folder

--- sendrecv/pipeline.sh
+++ sendrecv/pipeline.sh
@@ -2,6 +2,7 @@
 echo
 
 p=1
+m=5
 for i in {2..5}
     do
     echo $p
@@ -11,12 +12,12 @@ for i in {2..5}
 #sbatch pingpongSsend.in
     echo "" >> single.in
     echo "#SBATCH --nodes=2" >> single.in
-    echo "#SBATCH --job-name=calls"$p >> single.in
-    echo "#SBATCH --output=66calls"$p".out" >> single.in
+echo "#SBATCH --job-name="$m$m"pipe"$p >> single.in
+echo "#SBATCH --output="$m$m"pipe"$p".out" >> single.in
     echo "#SBATCH --ntasks-per-node=1" >> single.in
     echo "" >> single.in
     echo "" >> single.in
-    echo "mpirun --mca btl_openib_if_include mlx4_0 build/pingpong -s 6 -r 6 -o 2 -e 10000000000 -n" $p >> single.in
+    echo "mpirun --mca btl_openib_if_include mlx4_0 --mca btl_openib_eager_limit 136 build/pingpong -s "$m" -r "$m "-o 2 -e 10000000000 -p" $p >> single.in
     echo "" >> single.in
     echo "exit 0" >> single.in
     sbatch single.in
