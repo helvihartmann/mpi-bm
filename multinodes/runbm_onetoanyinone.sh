d37f7aab429d7966bc9ed80f2700b12906f36734
Helvi Hartmann <hartmann@login01.cm.cluster>
2014-10-31 11:37:40 +0100

    fixed ibtest to be parallel

--- multinodes/runbm_onetoanyinone.sh
+++ multinodes/runbm_onetoanyinone.sh
@@ -2,12 +2,13 @@
 
 q=0 #nature of pipe
 START=2
-ENDs=8
+ENDs=3
 i=2
 o=2
 m=2
 w=200
-e=16
+a=16
+e=18
 #b=8589934592
 b=4194304
 node="dual"
@@ -32,7 +33,7 @@ do
     echo "number of senders: " $s
     echo "number of processes n: " $n
 done
-    echo "testib.sh" $s $n ">> 55nodes_i"$i"_n"$n"_"$node".log" >> single.in
+    echo "multipleib_write_bw.sh" $s $n ">> 55nodes_i"$i"_n"$n"_"$node".log" >> single.in
     echo "exit 0" >> single.in
     cat single.in
     sbatch single.in
