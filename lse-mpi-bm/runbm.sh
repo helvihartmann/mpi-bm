0ac7347c75fe774be98b2b3cf0795d23ce08f256
Hartmann Helvi <hhartmann@code.compeng.uni-frankfurt.de>
2015-05-27 10:24:25 +0200

    started new benchmark run and removed runbm.sh from git repository, since it will change every time unsignificantly when doing a new bm run

--- lse-mpi-bm/runbm.sh
+++ lse-mpi-bm/runbm.sh
@@ -2,13 +2,13 @@
 
 q=0 #nature of pipe
 START=4
-END=28 #make it always even
+END=24 #make it always even
 stepsize=4
-i=4000
+i=10000
 o=2
 w=200
 a=16
-e=24
+e=22
 #b=8589934592
 b=17179869184
 node="dual"
@@ -21,7 +21,7 @@ echo "" >> single.in
 echo "#SBATCH --nodes="$END >> single.in
 #echo "#SBATCH --constraint="$node >> single.in
 echo "#SBATCH --job-name="$i"_"$END >> single.in
-echo "#SBATCH --nodefile=nodelist28.txt" >> single.in
+echo "#SBATCH --nodefile=nodelist24.txt" >> single.in
 echo "" >> single.in
 echo "" >> single.in
 
@@ -36,9 +36,9 @@ do
 
 
 #echo "testib_parallel.sh "$n" "$n" | tee 55nodes_s"$s"_n"$END"_m"$m".log" >> single.in
-    echo "mpirun --npernode 1 --mca btl_openib_if_include mlx4_0 build/multinodes -m 1 -i "$i" -w "$w" -e "$e" -o "$o" -q" $q "-s" $((n/2)) "-r" $((n/2)) "-b" $b ">> 55nodes_m1_s"$((n/2))"_n"$END"_"$node".out" >> single.in
+    echo "mpirun --npernode 1 --mca btl_openib_if_include mlx4_0 build/lse-mpi-bm -m 1 -i "$i" -w "$w" -e "$e" -o "$o" -q" $q "-s" $((n/2)) "-r" $((n/2)) "-b" $b ">> 55nodes_m1_s"$((n/2))"_n"$END"_"$node".out" >> single.in
 
-    echo "mpirun --npernode 2 --mca btl_openib_if_include mlx4_0 build/multinodes -m 2 -i "$i" -w "$w" -e "$e" -o "$o" -q" $q "-s" $n "-r" $n "-b" $b ">> 55nodes_m2_s"$n"_n"$END"_"$node".out" >> single.in
+    echo "mpirun --npernode 2 --mca btl_openib_if_include mlx4_0 build/lse-mpi-bm -m 2 -i "$i" -w "$w" -e "$e" -o "$o" -q" $q "-s" $n "-r" $n "-b" $b ">> 55nodes_m2_s"$n"_n"$END"_"$node".out" >> single.in
     echo "" >> single.in
 
     echo "number of nodes: " $END
